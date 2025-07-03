0204f8718e2e4e7bbe09d95182a32f5e610e4ce0
ZamZamGraphics <zamzamprintmedia@gmail.com>
2025-07-03 14:06:09 +0600

    change next config file

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,6 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
         protocol: "https",
         hostname: "*.almadinait.com",
       },
@@ -19,11 +15,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    APP_URL: "http://localhost:3000",
-    API_URL: "https://api.almadinait.com/wp-json/wp/v2",
-    NEXT_PUBLIC_SERVER_URL: "http://localhost:4000/v1/api",
-  }
 };
 
 export default withPlaiceholder(nextConfig);
